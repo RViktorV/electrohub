b35fa103bb6c7f2c6f0791e56ef0ddca21fc7d0b
vitek <rgavih@yandex.ru>
2024-12-04 23:49:15 +0700

    conservation 04.12.24 23:49

--- electronics_hub/apps.py
+++ electronics_hub/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ElectronicsHubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'electronics_hub'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "electronics_hub"
